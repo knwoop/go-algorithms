78e849f2249e65159ab32c2ac50002bdbaaef2da
kntaka <knt.takahashii@gmail.com>
2020-06-02 23:59:33 +0900

    fix

--- datastructure/binarytree/binarytree.go
+++ datastructure/binarytree/binarytree.go
@@ -1,75 +1,46 @@
 package bst
 
-type Node struct {
-	val                 int
-	parent, left, right *Node
+type Comparable func(c1 interface{}, c2 interface{}) bool
+
+type BinaryTree struct {
+	node        interface{}
+	left, right *BinaryTree
+
+	lessFun Comparable
 }
 
-func NewNode(i int) *Node {
-	return &Node{val: i}
+func New(compareFun Comparable) *BinaryTree {
+	return &BinaryTree{
+		node:    nil,
+		lessFun: compareFun,
+	}
 }
 
-func (n *Node) Compare(m *Node) int {
-	if n.val < m.val {
-		return -1
-	} else if n.val > m.val {
-		return 1
+func (tree *BinaryTree) Search(value interface{}) *BinaryTree {
+	if tree.node == nil {
+		return nil
+	}
+
+	if tree.node == value {
+		return tree
+	}
+	if tree.lessFun(value, tree.node) {
+		return tree.left.Search(value)
 	} else {
-		return 0
+		return tree.right.Search(value)
 	}
 }
 
-type Tree struct {
-	head *Node
-	size int
-}
-
-func NewTree(n *Node) *Tree {
-	if n == nil {
-		return &Tree{}
-	}
-	return &Tree{head: n, size: 1}
-}
-
-func (t *Tree) Insert(i int) {
-	n := &Node{val: i}
-	if t.head == nil {
-		t.head = n
-		t.size++
+func (tree *BinaryTree) Insert(value interface{}) {
+	if tree.node == nil {
+		tree.node = value
+		tree.right = New(tree.lessFun)
+		tree.left = New(tree.lessFun)
 		return
 	}
-
-	h := t.head
-
-	for {
-		if n.Compare(h) == -1 {
-			if h.left == nil {
-				h.left = n
-				n.parent = h
-				break
-			} else {
-				h = h.right
-			}
-		}
+	if tree.lessFun(value, tree.node) {
+		tree.left.Insert(value)
+	} else {
+		tree.right.Insert(value)
 	}
-	t.size++
-}
-
-func (t *Tree) Search(i int) *Node {
-	h := t.head
-	n := &Node{val: i}
-
-	for h != nil {
-		switch h.Compare(n) {
-		case -1:
-			h = h.right
-		case 1:
-			h = h.left
-		case 0:
-			return h
-		default:
-			panic("Node not found")
-		}
-	}
-	panic("Node not found")
 }
